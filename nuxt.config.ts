b0c702733177a8debdf05416b1c425adff35e414
Martyn Cook <martyn@solutions-online.co.nz>
2024-02-21 15:30:10 +1300

    2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,8 +46,8 @@ export default defineNuxtConfig({
         // },
       ],
       link: [
-        { href: "assets/img/favicon.png", rel: "icon" },
-        { href: "assets/img/apple-touch-icon.png", rel: "apple-touch-icon" },
+        { href: "/assets/img/favicon.png", rel: "icon" },
+        { href: "/assets/img/apple-touch-icon.png", rel: "apple-touch-icon" },
         {
           href: "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i",
           rel: "stylesheet",
